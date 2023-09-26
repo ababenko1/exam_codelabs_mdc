308c71a7f744cc9f97ef59c43c0ab4714e990075
ababenko1 <ababenko@griddynamics.com>
2023-09-26 10:09:46 +0200

    Added Login screen

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -1,10 +1,15 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.password_input
+import kotlinx.android.synthetic.main.shr_login_fragment.password_text
+import kotlinx.android.synthetic.main.shr_login_fragment.view.next_button
+import kotlinx.android.synthetic.main.shr_login_fragment.view.password_text
 
 /**
  * Fragment representing the login screen for Shrine.
@@ -18,8 +23,27 @@ class LoginFragment : Fragment() {
         // Snippet from "Navigate to the next Fragment" section goes here.
 
         val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+        view.next_button.setOnClickListener {
+            if (!isPasswordValid(password_text.text)) {
+                password_input.error = getString(R.string.shr_error_password)
+            } else {
+                password_input.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+
+        view.password_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_text.text)) {
+                password_input.error = null
+            }
+            false
+        }
+
         return view
     }
 
-    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?): Boolean {
+       return text != null && text.length >= 8
+    }
 }
