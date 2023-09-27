4db3f72061e3aef2451b85cf325862f2453d701a
ababenko1 <ababenko@griddynamics.com>
2023-09-27 09:23:41 +0200

    Added Material Structure and Layout

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -2,15 +2,45 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.app_bar
+import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.recycler_view
 
 class ProductGridFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
+        (activity as MainActivity).setSupportActionBar(view.app_bar)
+        initList(view)
+        return view
+    }
+
+    private fun initList(view: View) {
+        view.recycler_view.setHasFixedSize(true)
+        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        val adapter = ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+        view.recycler_view.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+        val itemDecoration = ProductGridItemDecoration(largePadding, smallPadding)
+        view.recycler_view.addItemDecoration(itemDecoration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.shr_toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
